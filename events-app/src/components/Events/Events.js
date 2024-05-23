10a8275d4c03d1b23cfa172c719d1782ba0500cb
Andrii <andriymyrosh@gmail.com>
2024-05-23 19:17:58 +0300

    Update event details, create and adjust register, login and logut

--- events-app/src/components/Events/Events.js
+++ events-app/src/components/Events/Events.js
@@ -1,25 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 import "../../styles.css"
+import EventCard from "../EventCard/EventCard";
 
-function Events (){
+function Events() {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const token = localStorage.getItem('access_token');
+                const response = await axios.get('http://localhost:5000/api/v1/events', {
+                    headers: {Authorization: `Bearer ${token}`}
+                });
+                setEvents(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
+
     return (
         <div className="events-page">
-            <h1 className="title-text"> Upcoming Events</h1>
+            <div className="title-container">
+                <h1 className="title-text">Upcoming Events</h1>
+                <div className="button-container">
+                    <Link to="/create-event" className="btn btn-primary">Create New Event</Link>
+                </div>
+            </div>
             <ul className="events-list">
-                <li className="event-card">
-                    <h2 className="event-title">Event Title</h2>
-                    <p className="event-details"><span>Date:</span> January 1, 2025</p>
-                    <p className="event-details"><span>Location:</span> Venue Name, City</p>
-                    <a href="#" className="event-link">View Details</a>
-                </li>
-                <li className="event-card">
-                    <h2 className="event-title">Event Title</h2>
-                    <p className="event-details"><span>Date:</span> January 2, 2025</p>
-                    <p className="event-details"><span>Location:</span> Venue Name, City</p>
-                    <a href="#" className="event-link">View Details</a>
-                </li>
+                {events.map(event => <EventCard key={event.id} event={event}/>)}
             </ul>
         </div>
-    )
+    );
+
 }
 
-export default Events
+export default Events;
---
+++ events-app/src/components/Register/Register.js
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import "../../styles.css"
+import { AuthContext } from "../../AuthContext";
+
+async function register(name, email, password) {
+    try {
+        const response = await axios.post('http://localhost:5000/api/v1/auth/register', {
+            name,
+            email,
+            password
+        });
+
+        const { data } = response;
+        localStorage.setItem('refresh_token', data.refresh_token);
+        localStorage.setItem('access_token', data.access_token);
+        localStorage.setItem('user_id', data.user_id);
+
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+function Register() {
+    const { setLoggedIn } = useContext(AuthContext);
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const result = await register(form.name, form.email, form.password);
+        if (result) {
+            setLoggedIn(true);
+            navigate('/events');
+        } else {
+            console.log('Registration failed: Check your credentials');
+        }
+    };
+
+    return (
+        <main className="register-page">
+            <div className="register-form">
+                <h2>Register</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Username</label>
+                        <input type="text" id="name" name="name" required onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" name="password" required onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Register</button>
+                </form>
+            </div>
+        </main>
+    );
+}
+
+export default Register;
