572b5e17e7b99c8aefd77c1371b72a70b7fcf7e4
Andrii <andriymyrosh@gmail.com>
2024-05-23 19:59:56 +0300

    Add edit_event functionality

--- events-app/src/components/EditEvent/EditEvent.js
+++ events-app/src/components/EditEvent/EditEvent.js
@@ -1,29 +1,78 @@
-import "../../styles.css"
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useLocation, useNavigate} from 'react-router-dom';
+import "../../styles.css";
 
-function EditEvent(props) {
+function EditEvent() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [eventData, setEventData] = useState(location.state.event || {});
+
+    const handleChange = (e) => {
+        setEventData({...eventData, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            console.log(eventData)
+            const token = localStorage.getItem('access_token');
+            const {description, location, title, date} = eventData;
+            const response = await axios.put(`http://localhost:5000/api/v1/events/${eventData.id}`, {
+                description,
+                location,
+                title,
+                date
+            }, {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            console.log(response.data);
+            navigate(`/event/${eventData.id}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        if (!location.state.event) {
+            // Fetch the event data if it's not passed through the state
+            const fetchEventData = async () => {
+                const response = await axios.get(`http://localhost:5000/api/v1/events/${location.state.eventId}`);
+                setEventData(response.data);
+            }
+
+            fetchEventData();
+        }
+    }, [location.state.event]);
+
     return (
-                <main className="edit-event">
+        <main className="edit-event">
             <section className="edit-event-form">
                 <h1>Edit Event</h1>
-                <form noValidate>
+                <form noValidate onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label for="event-title">Event Title</label>
-                        <input type="text" id="event-title" name="event-title" required placeholder={props.title}/>
+                        <label htmlFor="title">Event Title</label>
+                        <input type="text" id="title" name="title" required value={eventData.title}
+                               onChange={handleChange}/>
                     </div>
                     <div className="form-group">
-                        <label for="event-date">Event Date</label>
-                        <input type="date" id="event-date" name="event-date" required/>
+                        <label htmlFor="date">Event Date</label>
+                        <input type="date" id="date" name="date" required value={eventData.date}
+                               onChange={handleChange}/>
                     </div>
                     <div className="form-group">
-                        <label for="event-description">Event Description</label>
-                        <textarea id="event-description" name="event-description" rows="4" required></textarea>
+                        <label htmlFor="description">Event Description</label>
+                        <textarea id="description" name="description" rows="4" required value={eventData.description}
+                                  onChange={handleChange}></textarea>
                     </div>
                     <div className="form-group">
-                        <label for="event-location">Event Location</label>
-                        <input type="text" id="event-location" name="event-location" required/>
+                        <label htmlFor="location">Event Location</label>
+                        <input type="text" id="location" name="location" required value={eventData.location}
+                               onChange={handleChange}/>
                     </div>
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary">Save Changes</button>
+                        <button type="submit" className="btn btn-primary" style={{marginRight: '10px'}}>Save Changes
+                        </button>
                         <button type="button" className="btn btn-secondary">Cancel</button>
                     </div>
                 </form>
